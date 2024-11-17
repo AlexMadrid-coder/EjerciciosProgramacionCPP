a7dec2a794cfcc97f36f06182d130d564c46d013
marina <visema@alumni.uv.es>
2024-11-17 13:49:04 +0100

    ejercicio modulo 2

---
+++ Exercises/MODULE-2 ALGORITHMS/PR2/modulo2ej.cpp
@@ -0,0 +1,14 @@
+//
+// Created by marina on 17/11/2024.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+  int i;
+  cout << "hola" << endl;
+  for (i = 0; i < 10; i++) {
+    cout << i << endl;
+  }
+  return 0;
+}
